fe99d1ad456102db22d490817602ec1d71f76e07
csioza <csioza@163.com>
2020-07-12 13:15:20 +0800

    修改CMakeLists.txt

--- test/build_client.sh
+++ test/build_client.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# sed -i "s/tcpmotor/client/g" "./CMakeLists.txt"
-# sed -i "s/server/client/g" "./CMakeLists.txt"
-
-# sed -i "s/TEST_CPP/test_client.cpp/g" "./CMakeLists.txt"
-# sed -i "s/test_server.cpp/test_client.cpp/g" "./CMakeLists.txt"
-
 rm -rf CMakeLists.txt
 cp CMakeLists.client.txt CMakeLists.txt
 
